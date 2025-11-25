e5e68f978f7685b968c3c7487552a687d555ffc7
alex_di <mirag0505@yandex.ru>
2025-11-25 13:11:53 +0300

    6

--- 6.fs
+++ 6.fs
@@ -9,5 +9,5 @@ let rec isIthChar = fun (s: string,n,c) -> s.[n] = c
 // 17.3
 let rec occFromIth = function
     | (s, n, c) when n >= String.length s -> 0
-    | (s, n, c) when isIthChar(s, n, c) -> 1 + occFromIth(s, n + 1, c)
+    | (s, n, c) when s.[n] = c -> 1 + occFromIth(s, n + 1, c)
     | (s, n, c) -> occFromIth(s, n + 1, c)
